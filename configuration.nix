0623b2ac988f3e0eb91f61d8dfc0b71c36a9cf15
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-05-10 18:44:39 -0700

    configuration.nix: removed unused bindings

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -24,7 +24,11 @@
   users.users.feltfomo = {
     isNormalUser = true;
     hashedPassword = "$y$j9T$HT.mVqk50c03QSEv1rqlP0$5albZpdKB3hIndg.ecMfZ2ZxaDPEwDx5AbZKLaY9tY8";
-    extraGroups = [ "wheel" "networkmanager" "video" ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video"
+    ];
   };
 
   programs.hyprland = {
