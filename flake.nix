9718721437ba756ff51f270af07d66595dddf8df
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-05-11 23:59:04 -0700

    added home manager and manage fish shell with it

--- flake.nix
+++ flake.nix
@@ -18,15 +18,26 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
   };
 
   outputs =
     {
-      spicetify-nix,
       home-manager,
       impermanence,
+      lix-module,
       nixpkgs,
       disko,
+      lix,
       ...
     }@inputs:
     let
@@ -38,6 +49,7 @@
         specialArgs = { inherit inputs; };
         modules = [
           impermanence.nixosModules.impermanence
+          lix-module.nixosModules.default
           disko.nixosModules.disko
           ./hosts/lumi/configuration.nix
         ];
