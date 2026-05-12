3e584b449a408429a56d5ad5eeb780deb043d760
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-05-12 00:12:12 -0700

    moved user programs to home.nix

--- home.nix
+++ home.nix
@@ -5,12 +5,19 @@
     ./home/spicetify.nix
     inputs.spicetify-nix.homeManagerModules.spicetify
   ];
+
   home.username = "feltfomo";
   home.homeDirectory = "/home/feltfomo";
 
   home.packages = with pkgs; [
+    kitty
+    brave
+    fuzzel
     firefox
     librewolf
+    fastfetch
+    zed-editor
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
   programs.git.settings = {
--- modules/packages.nix
+++ modules/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 {
   environment = {
     systemPackages = with pkgs; [
@@ -9,21 +9,13 @@
       glib
       wget
       curl
-      kitty
-      brave
-      fuzzel
-      fastfetch
       grub2_efi
       efibootmgr
-      zed-editor
       wl-clipboard
       libsForQt5.qt5ct
       qt6Packages.qt6ct
-      catppuccin-kvantum
-      rose-pine-kvantum
       gsettings-desktop-schemas
       libsForQt5.qtstyleplugin-kvantum
-      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
   };
 }
