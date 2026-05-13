c6e5dee15a6fbf292d29653c7d45e4cd649747b9
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-05-13 11:09:22 -0700

    made gtk icons papirus dark

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   gtk = {
     enable = true;
     colorScheme = "dark";
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
   };
 }
