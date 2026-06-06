b0f50624bf515bde572c8e27e9c13086e36beded
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-06-06 08:23:53 -0700

    notion app dont work

--- modules/users/feltfomo.nix
+++ modules/users/feltfomo.nix
@@ -57,7 +57,6 @@
           ayugram-desktop
           translate-shell
           jetbrains.idea-oss
-          notion-app-enhanced
           inputs.walker.packages.${system}.default
           inputs.zen-browser.packages."${system}".twilight
           inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
