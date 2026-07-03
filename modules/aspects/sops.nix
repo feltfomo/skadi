69381272663ec29640153f5b8bb35a3920ea76db
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-07-03 12:03:12 -0700

    sops: disable unused gnupg ssh key path (fixes RSA->GPG import error in initrd)

--- modules/aspects/sops.nix
+++ modules/aspects/sops.nix
@@ -8,6 +8,8 @@
         defaultSopsFile = ../../secrets/secrets.yaml;
         validateSopsFiles = false;
         age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
+        # decrypt via age only; stops the RSA->GPG import that fails in initrd
+        gnupg.sshKeyPaths = [ ];
         secrets = {
           "feltfomo-password".neededForUsers = true;
           "notion-token" = {
