48276ec8fd8d474a8e7deab2dbf0b2a645f41d95
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-07-26 11:25:17 -0700

    init.lua: set 2-space indent

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -4,4 +4,11 @@ vim.g.maplocalleader = " "
 
 vim.opt.clipboard = "unnamedplus"
 
+-- shiftwidth defaults to tabstop (8) when unset, and treesitter's indentexpr
+-- multiplies by it, so nix's nested attrsets blow up without this.
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
 require("config.lazy")
