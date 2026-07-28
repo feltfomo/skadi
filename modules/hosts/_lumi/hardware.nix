b5e883824f34f6195926b14eb956dc03e8ba9b5d
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-07-28 08:17:05 -0700

    lumi: sof firmware and audio suspend fixes for working sound

--- modules/hosts/_lumi/hardware.nix
+++ modules/hosts/_lumi/hardware.nix
@@ -1,4 +1,9 @@
-{ lib, modulesPath, ... }:
+{
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}:
 {
   # auto-detected hardware modules
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
@@ -16,6 +21,29 @@
 
   # keep intel cpu microcode up to date
   hardware.cpu.intel.updateMicrocode = true;
+  hardware.firmware = [ pkgs.sof-firmware ];
+  boot.kernelParams = [ "snd_intel_dspcfg.dsp_driver=3" ];
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="pci", KERNEL=="0000:00:1f.3", ATTR{power/control}="on"
+  '';
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  services.pipewire.wireplumber.extraConfig."51-disable-suspend" = {
+    "monitor.alsa.rules" = [
+      {
+        matches = [
+          { "node.name" = "~alsa_output.*"; }
+          { "node.name" = "~alsa_input.*"; }
+        ];
+        actions = {
+          update-props = {
+            "session.suspend-timeout-seconds" = 0;
+          };
+        };
+      }
+    ];
+  };
+
 }
