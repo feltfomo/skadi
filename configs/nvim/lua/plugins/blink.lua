26fb6b2685412d4a4185be298cab76b7e04df771
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-08-08 19:19:31 -0700

    nvim: customized blink with borders

--- configs/nvim/lua/plugins/blink.lua
+++ configs/nvim/lua/plugins/blink.lua
@@ -1,18 +1,34 @@
 return {
-	"saghen/blink.cmp",
-	-- pinned to a release so the rust fuzzy matcher ships prebuilt, no local cargo build needed
-	version = "1.*",
-	event = "InsertEnter",
-	opts = {
-		keymap = { preset = "default" },
-		appearance = {
-			nerd_font_variant = "mono",
-		},
-		completion = {
-			ghost_text = { enabled = true },
-		},
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
-	},
+  "saghen/blink.cmp",
+  dependencies = { 'rafamadriz/friendly-snippets' },
+  -- pinned to a release so the rust fuzzy matcher ships prebuilt, no local cargo build needed
+  version = "1.*",
+  --@module 'blink.cmp'
+  --@type blink.cmp.Config
+  event = "InsertEnter",
+  opts = {
+    keymap = { preset = "default" },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    completion = {
+      ghost_text = { enabled = true },
+      menu = {
+        border = 'rounded'
+      },
+      documentation = {
+        window = {
+          border = 'rounded'
+        },
+      },
+    },
+    signature = {
+      window = {
+        border = 'rounded'
+      },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+  },
 }
