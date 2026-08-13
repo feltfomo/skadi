4791e74a1e0e6cf76089af3c3df947fdd621e726
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-08-12 20:12:44 -0700

    qt: made my own templates instead of using shells

--- modules/aspects/qt-hm.nix
+++ modules/aspects/qt-hm.nix
@@ -1,26 +1,66 @@
+{ program, rootPath, ... }:
 {
-  den.aspects.qt-hm.homeManager =
-    { config, ... }:
-    {
-      qt = {
-        enable = true;
-        platformTheme.name = "qtct";
+  den.aspects.qt-hm = program {
+    imports = [
+      (
+        { config, ... }:
+        {
+          qt = {
+            enable = true;
+            platformTheme.name = "qtct";
 
-        qt5ctSettings = {
-          Appearance = {
-            color_scheme_path = "${config.home.homeDirectory}/.config/qt5ct/colors/noctalia.conf";
-            custom_palette = true;
-            style = "Fusion";
-          };
-        };
+            qt5ctSettings.Appearance = {
+              color_scheme_path = "${config.home.homeDirectory}/.config/qt5ct/colors/reactive.conf";
+              custom_palette = true;
+              style = "Fusion";
+            };
 
-        qt6ctSettings = {
-          Appearance = {
-            color_scheme_path = "${config.home.homeDirectory}/.config/qt6ct/colors/noctalia.conf";
-            custom_palette = true;
-            style = "Fusion";
+            qt6ctSettings.Appearance = {
+              color_scheme_path = "${config.home.homeDirectory}/.config/qt6ct/colors/reactive.conf";
+              custom_palette = true;
+              style = "Fusion";
+            };
           };
-        };
-      };
+        }
+      )
+    ];
+
+    theme = {
+      id = "qt";
+      templates = [
+        {
+          subId = "qt5ct";
+          output = ".config/qt5ct/colors/reactive.conf";
+          placedAs = "qt5ct.conf";
+          renderers = {
+            noctalia = {
+              source = "${rootPath}/configs/qt/reactive.conf";
+              sharedWith = [
+                "dms"
+                "illogical-impulse"
+                "end4-pc"
+              ];
+            };
+            caelestia.source = "${rootPath}/configs/qt/caelestia.conf";
+          };
+        }
+        {
+          subId = "qt6ct";
+          output = ".config/qt6ct/colors/reactive.conf";
+          placedAs = "qt6ct.conf";
+          renderers = {
+            noctalia = {
+              source = "${rootPath}/configs/qt/reactive.conf";
+              sharedWith = [
+                "dms"
+                "illogical-impulse"
+                "end4-pc"
+              ];
+            };
+            caelestia.source = "${rootPath}/configs/qt/caelestia.conf";
+          };
+        }
+      ];
     };
+  };
 }
